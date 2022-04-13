907acad9dac087ae86118b1143fab93d1453f930
AlymiReyna <alymireyna22@gmail.com>
2022-04-12 23:45:37 -0500

    Noveno commit Example9

---
+++ weekly_mission_1/example9/pokemon.js
@@ -0,0 +1,16 @@
+class Pokemon {
+    constructor (name) {
+      this.name = name
+    }
+  
+    sayHello () {
+      console.log(`¡Mi pokemon ${this.name} te saluda!`)
+    }
+
+    sayMessage (message) {
+        console.log(`Mi pokemon ${this.name} te dice: ${message}`)
+      }
+
+  }
+
+  module.exports = Pokemon
