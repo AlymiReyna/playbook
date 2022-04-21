287ce78a1569e2a378c5103d62c43394415fa984
AlymiReyna <alymireyna22@gmail.com>
2022-04-21 16:40:33 -0500

    Add : Modelando GitHub con definición de clases

--- weekly_mission_2/exercises/exercise_3/modeladoDeObjetosConDefinicionDeClases.js
+++ weekly_mission_2/exercises/exercise_3/modeladoDeObjetosConDefinicionDeClases.js
@@ -21,4 +21,46 @@ class Repo{
       }
 }
 
-console.log(getGeneralInfo())
+//Instanciando objeto a la clase Repo
+/*const obj = new Repo("LaunchX", "Alymi", "English", 16, 20, 11, 2, 1)
+console.log(obj.getGeneralInfo())*/
+
+class Issue{
+    constructor(title, repositoryNameAssociated,status, numberOfCommits, labels, author, date_created, date_updated){
+        this.title = title,
+        this.repositoryNameAssociated = repositoryNameAssociated,
+        this.status = status,
+        this.numberOfCommits = numberOfCommits,
+        this.labels = labels,
+        this.author = author,
+        this.date_created = date_created,
+        this.date_updated = date_updated
+    }
+
+    getTitleAndAuthor(){
+        return this.title + " by " + this.author
+      }
+
+      getGeneralInfo(){
+        return `This repository ${this.title} was created by ${this.author}`
+      }
+}
+
+class PullRequest{
+    constructor(title, branchName, date_created, status, repositoryNameAssociated, author){
+        this.title = title,
+        this.branchName = branchName,
+        this.date_created = date_created,
+        this.status = status,
+        this.repositoryNameAssociated = repositoryNameAssociated,
+        this.author = author
+    }
+
+    getStatus(){
+        return this.title + " is currently " + this.status
+      }
+
+    getTitleAndAuthor(){
+          return this.title + " by " + this.author
+        }
+}
